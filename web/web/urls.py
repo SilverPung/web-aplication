3b6e375f18f6d834f6f49e184c1928ba184687cd
SilverPung <kristofer.pawel@gmail.com>
2024-03-03 22:35:15 +0100

    dodanie wyświetlanie item

--- web/web/urls.py
+++ web/web/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static  #nie robić tego w produkcji
+
 from django.contrib import admin
 from django.urls import path
 from core.views import index,contact
@@ -22,4 +25,4 @@ urlpatterns = [
     path('', index, name='index'),#strona główna
     path('contact/', contact, name='contact'),#kontakt
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #nie robić tego w produkcji
