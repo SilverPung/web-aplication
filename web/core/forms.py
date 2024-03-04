e2954e68337cb94d6862c3ff232fe5953f55c321
SilverPung <kristofer.pawel@gmail.com>
2024-03-04 22:03:45 +0100

    stworzenie menu po zalogowaniu

--- web/core/forms.py
+++ web/core/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+class LoginForm(AuthenticationForm):#form do logowania na stronie
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your Username', 'class': 'w-full p-2 my-2 mx-5 border border-gray-300 rounded-xl'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Your Password', 'class': 'w-full p-2 my-2 mx-5 border border-gray-300 rounded-xl'}))
+
+
+
 
 class SignupForm(UserCreationForm):#form do rejestracji na stronie
     class Meta:#klasa do formularza
@@ -11,3 +19,4 @@ class SignupForm(UserCreationForm):#form do rejestracji na stronie
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Your Email', 'class': 'w-full p-2 my-2 mx-5 border border-gray-300 rounded-xl'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Your Password', 'class': 'w-full p-2 my-2 mx-5 border border-gray-300 rounded-xl'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password', 'class': 'w-full p-2 my-2 mx-5 border border-gray-300 rounded-xl'}))
+
--- web/core/urls.py
+++ web/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 app_name = 'core' #nazwa podaplikacji
 
 urlpatterns = [ #adresy url na stronie odpowiadające za wywołanie funkcji z pliku views.py
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login'),
 ]
