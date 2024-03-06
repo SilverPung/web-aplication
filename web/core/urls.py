501a6fee3cd1317be5665962e0ea86d75244e2de
SilverPung <kristofer.pawel@gmail.com>
2024-03-06 21:47:25 +0100

    stworzenie strony na wylogowywanie

--- web/core/urls.py
+++ web/core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [ #adresy url na stronie odpowiadające za wywołanie funkcji z pl
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html',authentication_form=LoginForm), name='login'),
 ]
--- web/core/views.py
+++ web/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from item.models import Category, Item
 from .forms import SignupForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 # Create your views here.
 # funkcje odpowiadające za wyświetlanie stron wywyują się po wpisaniu odpowiedniego adresu url z pliku urls.py
 def index(request):#funkcja odpowiadająca za wyświetlanie strony głównej
@@ -20,3 +22,11 @@ def signup(request):#funkcja odpowiadająca za wyświetlanie strony rejestracji
     else:
         form = SignupForm()
     return render(request, 'core/signup.html', {'form': form})#wyświetlenie strony rejestracji
+
+
+def logout_view(request):#funkcja odpowiadająca za wylogowanie
+    if request.method == 'POST':
+        logout(request)
+        return redirect('/')
+    return render(request, 'core/logout.html')#wyświetlenie strony wylogowania
+
